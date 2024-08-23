0910dbbcea0bfe903b3c3cb1443a471790d815e9
jangjinhyuk <jeww1234@gmail.com>
2024-08-23 22:39:38 +0900

    hihi

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -6,22 +6,23 @@ import { useSearchParams } from "react-router-dom";
 
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
-  let [query, setQuery] = useSearchParams()
-  const getProducts = useCallback( async () => {
-    let searchQuery=query.get("q")||""
-    console.log(query)
+  let [query, setQuery] = useSearchParams();
+  const getProducts = useCallback(async () => {
+    let searchQuery = query.get("q") || "";
+    console.log(query);
     let url = `http://localhost:5000/products?q=${searchQuery}`;
-    console.log("url", url)
+    console.log("url", url);
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
-  },[query]);
-  setQuery = ()=>{
-    console.log("what")
-  }
+  }, [query]);
+  setQuery = //eslint-disable-line no-unused-vars
+    () => {
+      console.log("what");
+    };
   useEffect(() => {
     getProducts();
-  },[query, getProducts]);
+  }, [query, getProducts]);
 
   return (
     <div>
